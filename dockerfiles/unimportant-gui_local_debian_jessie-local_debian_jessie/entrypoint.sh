c79e9525903c4ad66b42fbdfa9a1d65aafa4e377
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2017-04-03 17:27:44 -0400

    Code cleanup.

--- dockerfiles/unimportant-gui_local_debian_jessie-local_debian_jessie/entrypoint.sh
+++ dockerfiles/unimportant-gui_local_debian_jessie-local_debian_jessie/entrypoint.sh
@@ -6,7 +6,7 @@
 
 USER_ID=${LOCAL_USER_ID:-9001}
 
-echo "Starting with UID : $USER_ID"
+#echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user >/dev/null 2>&1
 export HOME=/home/user
 
