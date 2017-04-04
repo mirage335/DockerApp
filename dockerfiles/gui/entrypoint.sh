f3ff88b8b46ed95e8dcdf138e4876f6da6ca319a
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2017-04-04 01:36:55 -0400

    Hook root during launch, to allow modifications.

--- dockerfiles/gui/entrypoint.sh
+++ dockerfiles/gui/entrypoint.sh
@@ -6,8 +6,22 @@
 
 USER_ID=${LOCAL_USER_ID:-9001}
 
+if [[ "$LOCAL_USER_ID" == "" ]]
+then
+	#Root access by default.
+	if [[ "$1" == "" ]]
+	then
+		/bin/bash "$@"
+		exit
+	fi
+	
+	"$@"
+	exit
+fi
+
 #echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user >/dev/null 2>&1
+usermod -a -G video user
 export HOME=/home/user
 
 chown user:user "$HOME"
@@ -22,4 +36,5 @@ then
 fi
 
 exec /usr/local/bin/gosu user "$@"
+
 exit
